08f25580f8b816829fdc26bca4f4028b3deb016b
Vitor <v.e.silva3007@gmail.com>
2026-07-03 11:42:52 -0300

    pequena correcao no schema

--- cantinho-backend/servidor/schema.js
+++ cantinho-backend/servidor/schema.js
@@ -5,30 +5,29 @@ async function garantirSchema() {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS usuarios (
     id SERIAL PRIMARY KEY,
-    nome_usuario TEXT NOT NULL UNIQUE
+    nome_usuario TEXT NOT NULL UNIQUE,
     senha_hash TEXT NOT NULL,
-    role TEXT NOT NULL CHECK role in ('garcom', 'cozinha', 'admin')
+    role TEXT NOT NULL CHECK (role in ('garcom', 'cozinha', 'admin')),
     criado_em TIMESTAMPZ DEFAULT NOW()
-    )
+    );
 
     CREATE TABLE IF NOT EXISTS mesas (
       id SERIAL PRIMARY KEY,
       numero INTEGER NOT NULL UNIQUE,
-      preco NUMERIC NOT NULL,
-      categoria TEXT NOT NULL
+      status TEXT NOT NULL DEFAULT 'disponivel' CHECK (status in ('disponivel', 'reservada'))
     );
 
     CREATE TABLE IF NOT EXISTS itens_cardapio (
       id SERIAL PRIMARY KEY,
-      nome TEXT NOT NULL,
-      preco NUMERIC NOT NULL,
+      mesa_id INT NOT NULL REFERENCES mesas(id) ON DELETE RESTRICT,
+      preco NUMERIC NOT NULL CHECK (preco > 0 ),
       categoria TEXT NOT NULL
     );
 
     CREATE TABLE IF NOT EXISTS comandas (
       id SERIAL PRIMARY KEY,
-      numero_mesa INT NOT NULL,
-      status TEXT NOT NULL DEFAULT 'aberta',
+      mesa_id INT NOT NULL REFERENCES mesas(id) ON DELETE RESTRICT,
+      status TEXT NOT NULL DEFAULT 'aberta' CHECK (status in ('aberta', 'fechada')),
       data_abertura TIMESTAMP DEFAULT NOW(),
       valor_total NUMERIC DEFAULT 0
     );
@@ -37,9 +36,16 @@ async function garantirSchema() {
     ON comandas (numero_mesa)
     WHERE status = 'aberta';
 
+    CREATE TABLE IF NOT EXISTS pedidos (
+      id SERIAL PRIMARY KEY,
+      comanda_id INT REFERENCES comandas(id) ON DELETE RESTRICT,
+      status TEXT NOT NULL DEFAULT 'pendente' CHECK (status in ('pendente', 'pronto', 'entregue')),
+      criado_em TIMESTAMP DEFAULT NOW(),
+    );
+
     CREATE TABLE IF NOT EXISTS itens_comanda (
       id SERIAL PRIMARY KEY,
-      comanda_id INT REFERENCES comandas(id),
+      comanda_id INT REFERENCES comandas(id) ON DELETE RESTRICT,
       item_cardapio_id INT REFERENCES itens_cardapio(id),
       quantidade INT NOT NULL,
       observacao TEXT
